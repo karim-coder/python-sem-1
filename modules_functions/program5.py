6c7e2470ff2546caca34034931cf0637e1a7c911
karim-amiri <ab.amiri484@gmail.com>
2022-10-31 10:26:11 +0530

    program 5 add

---
+++ modules_functions/program5.py
@@ -0,0 +1,17 @@
+
+from itertools import count
+
+
+print("""
+Q) Write a Python program to create Fibonacci series upto n using Lambda.
+
+Solution:-
+""")
+
+n= int(input("Enter Fibonacci series length: "))
+
+fib_list=[0,1]
+
+any(map(lambda _:fib_list.append(sum(fib_list[-2:])), range(2,n)))
+
+print("Fibonacci series: ",fib_list)
